0725f09da8ef4383e5424b266decdc50a968b51c
이원형 <67450169+whl0526@users.noreply.github.com>
2022-12-27 15:49:41 +0900

    [Feat] #3 HomeMenuView 구현

---
+++ big-project-a-customer-ios/big-project-a-customer-ios/NotebookView.swift
@@ -0,0 +1,20 @@
+//
+//  NotebookView.swift
+//  big-project-a-customer-ios
+//
+//  Created by 이원형 on 2022/12/27.
+//
+
+import SwiftUI
+
+struct NotebookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotebookView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotebookView()
+    }
+}
