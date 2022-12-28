6bd7138ad57d81b86e0938964eddf8592db252c2
dahae0320 <ldh990320ldh@gmail.com>
2022-12-28 18:01:51 +0900

    [Feat] Create User, Register User, 이메일 중복 검사 기능 추가

---
+++ big-project-a-customer-ios/big-project-a-customer-ios/Store/SignUpViewModel.swift
@@ -0,0 +1,82 @@
+//
+//  SignUpViewModel.swift
+//  big-project-a-customer-ios
+//
+//  Created by 박민주 on 2022/12/27.
+//
+
+import Foundation
+import Firebase
+
+enum AuthenticationState {
+    case unauthenticated
+    case authenticating
+    case authenticated
+}
+
+class SignUpViewModel: ObservableObject {
+    @Published var errorMessage = ""
+    @Published var authenticationState: AuthenticationState = .unauthenticated
+    
+    let database = Firestore.firestore()
+    let authentification = Auth.auth()
+    
+    
+    // MARK: - Create New Customer(user)
+    /// Auth에 새로운 사용자를 생성합니다.
+    /// - Parameter email: 입력받은 사용자의 email
+    /// - Parameter password: 입력받은 사용자의 password
+    /// - Parameter nickname: 입력받은 사용자의 nickname
+    @MainActor
+    func createUser(email: String, password: String, nickname: String) async -> Bool {
+        authenticationState = .authenticating
+        do  {
+            try await authentification.createUser(withEmail: email, password: password)
+            print("account created.")
+            errorMessage = ""
+            authenticationState = .authenticated
+            // firestore에 user 등록
+            let currentUserId = authentification.currentUser?.uid ?? ""
+            registerUser(uid: currentUserId, email: email, nickname: nickname)
+            return true
+        }
+        catch {
+            print(error.localizedDescription)
+            errorMessage = error.localizedDescription
+            authenticationState = .unauthenticated
+            return false
+        }
+    }
+
+    // MARK: - Register Customer(user) in Firestore
+    /// Auth에 새롭게 만든 사용자 정보를 Firestore에 등록합니다.
+    /// - Parameter uid: 현재 사용자의 Auth uid
+    /// - Parameter email: 현재 사용자의 email
+    /// - Parameter nickname: 현재 사용자의 nickname
+    func registerUser(uid: String, email: String, nickname: String) {
+        database.collection("CustomerInfo")
+            .document(uid)
+            .setData([
+                "id" : uid,
+                "userEmail" : email,
+                "userNickname" : nickname
+            ])
+    }
+    
+    // MARK: - isEmailDuplicated
+    /// 사용자가 입력한 이메일이 이미 사용하고 있는지 검사합니다.
+    /// 입력받은 이메일이 DB에 이미 있다면 false를, 그렇지 않다면 true를 반환합니다.
+    /// - Parameter currentUserEmail: 입력받은 사용자 이메일
+    /// - Returns: 중복된 이메일이 있는지에 대한 Boolean 값
+    func isEmailDuplicated(currentUserEmail: String) async -> Bool {
+        do {
+            let document = try await database.collection("CustomerInfo")
+                .whereField("userEmail", isEqualTo: currentUserEmail)
+                .getDocuments()
+            return !(document.isEmpty)
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+    }
+}
