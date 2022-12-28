a06396e0e8bee11c8ca4d86faaeebcb15c8a7ef9
이원형 <67450169+whl0526@users.noreply.github.com>
2022-12-28 11:32:18 +0900

    [Chore] HomeMenuView, & SearchableBar fix #3

--- big-project-a-customer-ios/big-project-a-customer-ios/FolderHomeMenu/HomeMenuView.swift
+++ big-project-a-customer-ios/big-project-a-customer-ios/FolderHomeMenu/HomeMenuView.swift
@@ -1,10 +1,7 @@
 //
-//  HomeView.swift
-//  ProjectA-HomeMenu
+//  HomeMenuView.swift
 //
 //  Created by 이원형 on 2022/12/27.
-//
-
 import SwiftUI
 
 // MARK: Cell별 카테고리를 관리하기 위한 구조체
@@ -14,18 +11,21 @@ struct Item: Identifiable{
     let categoryImages: String
 }
 
-struct HomeMenuView: View {
-    @State private var searchText = ""
-    let items = [
-        Item(categoryTitles: "노트북", categoryImages: "Notebook"),
-        Item(categoryTitles: "휴대폰", categoryImages: "Phone"),
-        Item(categoryTitles: "PC", categoryImages: "Desktop"),
-        Item(categoryTitles: "모니터", categoryImages: "Monitor"),
-        Item(categoryTitles: "태블릿", categoryImages: "Tablet"),
-        Item(categoryTitles: "스마트워치", categoryImages: "Watch"),
+var items = [
+    Item(categoryTitles: "노트북", categoryImages: "Notebook"),
+    Item(categoryTitles: "휴대폰", categoryImages: "Phone"),
+    Item(categoryTitles: "PC", categoryImages: "Desktop"),
+    Item(categoryTitles: "모니터", categoryImages: "Monitor"),
+    Item(categoryTitles: "태블릿", categoryImages: "Tablet"),
+    Item(categoryTitles: "스마트워치", categoryImages: "Watch"),
+    
+]
 
-        ]
-   
+
+struct HomeMenuView: View {
+    @State var searchItem = items
+    @State private var searchText = ""
+    
     let columns = [
         GridItem(.fixed(170)),
         GridItem(.fixed(170))
@@ -33,48 +33,51 @@ struct HomeMenuView: View {
     
     var body: some View{
         NavigationStack {
-            VStack{
-            }
-            .searchable(text: $searchText)
-            .padding(.top,30)
-            ScrollView{
-                LazyVGrid(columns: columns, spacing: 15){
-                    ForEach(items) { item in
-                            NavigationLink {
-                                NotebookView()
-                            } label:{
+            ScrollView(.vertical){
+                LazyVGrid(columns: columns, spacing: 10){
+                    ForEach(searchItem) { item in
+                        NavigationLink {
+                            NotebookView()
+                        } label:{
                             ItemView(item: item)
                         }
+                    }//ForEach
+                }//LazyVGrid
+                .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+                .onChange(of: searchText) { index in
+                    if !index.isEmpty{
+                        searchItem = items.filter { $0.categoryTitles.contains(index) }
+                    } else {
+                        searchItem = items
                     }
-                    }
-                .padding(10)
-                }
-            .background(.white)
-            }
-        }
+                }//.onChange
+            }//ScrollView
+        }//NavigationStack
+        .background(.white)
+    }
     // MARK: 홈 메뉴에 그려질 Cell View
     struct ItemView: View{
         let item: Item
         var body: some View{
-          
-                VStack(spacing: 0){
-                    Text(item.categoryTitles)
-                        .font(.system(.title3 , weight: .bold))
-                        .foregroundColor(Color.black.opacity(0.8))
-                    
-                    Image(item.categoryImages)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:85, height: 85)
-                }
-                .frame(width:160 , height: 160)
-                .background(Color.white)
-                .cornerRadius(20)
-                .shadow(color: Color.black.opacity(0.2), radius: 5, y: 5)
+            
+            VStack(spacing: 0){
+                Text(item.categoryTitles)
+                    .font(.system(.title3 , weight: .bold))
+                    .foregroundColor(Color.black.opacity(0.8))
+                
+                Image(item.categoryImages)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width:85, height: 85)
             }
+            .frame(width:160 , height: 160)
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(color: Color.black.opacity(0.2), radius: 5, y: 5)
+        }
     }
-    }
+}
 //struct HomeView_Previews: PreviewProvider {
 //    static var previews: some View {
-//        HomeView()
+//        HomeMenuView()
 //    }
