87c0d6aa90e25f67cf98564f82cadd74e95e508c
jeonghoonji <73868968+jeonghoonji@users.noreply.github.com>
2022-12-28 17:49:06 +0900

    Revert "221228 product detail dohyeon"

--- big-project-a-customer-ios/big-project-a-customer-ios/ContentView.swift
+++ big-project-a-customer-ios/big-project-a-customer-ios/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            ProductDetailView().tabItem {
+            HomeMenuView().tabItem {
                 Image(systemName: "house")
                 Text("홈")
             }.tag(1)
