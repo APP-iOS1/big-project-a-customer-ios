142cb0364efc34ea21317013dbbfcae94d39fcb7
이원형 <67450169+whl0526@users.noreply.github.com>
2022-12-28 14:00:03 +0900

    [Chore] HomeMenuView & NotebookView 수정 및 ProductDetailView 연동 #3

--- big-project-a-customer-ios/big-project-a-customer-ios/FolderHomeMenu/NotebookView.swift
+++ big-project-a-customer-ios/big-project-a-customer-ios/FolderHomeMenu/NotebookView.swift
@@ -7,14 +7,85 @@
 
 import SwiftUI
 
-struct NotebookView: View {
-    var body: some View {
-        Text("This is Notebook View")
-    }
+
+struct NotebookItem: Identifiable{
+    let id = UUID()
+    let NotebookTitles: String
+    let NotebookImages: String
+    let NotebookPrices: String
 }
 
-struct NotebookView_Previews: PreviewProvider {
-    static var previews: some View {
-        NotebookView()
+var NotebookItems = [
+    NotebookItem(NotebookTitles: "MacBookPro 14", NotebookImages: "Notebook", NotebookPrices: "₩2,690,000"),
+    NotebookItem(NotebookTitles: "MacBookPro 13", NotebookImages: "Notebook", NotebookPrices: "₩2,490,000"),
+    NotebookItem(NotebookTitles: "MacBookPro 16", NotebookImages: "Notebook", NotebookPrices: "₩3,090,000"),
+    NotebookItem(NotebookTitles: "MacBookAir 13", NotebookImages: "Notebook", NotebookPrices: "₩1,690,000"),
+    NotebookItem(NotebookTitles: "MacBookAir 13", NotebookImages: "Notebook", NotebookPrices: "₩1,690,000"),
+    NotebookItem(NotebookTitles: "MacBookPro 14", NotebookImages: "Notebook", NotebookPrices: "₩2,690,000"),
+    NotebookItem(NotebookTitles: "MacBookPro 16", NotebookImages: "Notebook", NotebookPrices: "₩3,790,000"),
+    NotebookItem(NotebookTitles: "MacBookPro 16", NotebookImages: "Notebook", NotebookPrices: "₩3,090,000"),
+]
+
+struct NotebookView: View {
+    @State var searchNotebookItem = NotebookItems
+    @State private var searchText = ""
+    
+    let columns = [
+           GridItem(.fixed(170)),
+           GridItem(.fixed(170))
+       ]
+       
+    
+    var body: some View{
+        NavigationStack {
+            ScrollView(.vertical){
+                LazyVGrid(columns: columns, spacing: 10){
+                    ForEach(searchNotebookItem) { item in
+                        NavigationLink {
+                            ProductDetailView()
+                        } label:{
+                            ItemView(item: item)
+                        }
+                    }//ForEach
+                }//LazyVGrid
+                .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+                .onChange(of: searchText) { index in
+                    if !index.isEmpty{
+                        searchNotebookItem = NotebookItems.filter { $0.NotebookTitles.localizedStandardContains(index) }
+                    } else {
+                        searchNotebookItem = NotebookItems
+                    }
+                }//.onChange
+            }//ScrollView
+            .navigationTitle("Mac")
+        }//NavigationStack
+      
+        .background(.white)
+    }
+    // MARK: 홈 메뉴에 그려질 Cell View
+    struct ItemView: View{
+        let item: NotebookItem
+        var body: some View{
+            
+            VStack(spacing: 0){
+
+                Image(item.NotebookImages)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width:85, height: 85)
+                
+                Text(item.NotebookTitles)
+                    .font(.system(.title3 , weight: .bold))
+                    .foregroundColor(Color.black.opacity(0.8))
+                
+                Text(item.NotebookPrices)
+                    .font(.system(.footnote , weight: .bold))
+                    .foregroundColor(Color.blue.opacity(0.8))
+            }
+            .frame(width:160 , height: 160)
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(color: Color.black.opacity(0.2), radius: 5, y: 5)
+        }
     }
 }
