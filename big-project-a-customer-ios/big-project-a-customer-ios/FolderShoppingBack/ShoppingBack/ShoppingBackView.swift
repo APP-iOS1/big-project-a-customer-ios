53cc276d69d186423a8d1f801f69cd5708adb3db
jeonghoonji <wlwjdgns0920@naver.com>
2023-01-02 17:29:33 +0900

    [Fix] 소괄호 추가 수정

--- big-project-a-customer-ios/big-project-a-customer-ios/FolderShoppingBack/ShoppingBack/ShoppingBackView.swift
+++ big-project-a-customer-ios/big-project-a-customer-ios/FolderShoppingBack/ShoppingBack/ShoppingBackView.swift
@@ -166,7 +166,7 @@ struct ShoppingBackView: View {
 
                             }
                             
-                        }
+                        )}
                     }
                 }
                 .padding()
