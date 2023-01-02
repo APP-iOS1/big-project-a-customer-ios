ccf1551536d3af1830780a098493d8e52426937e
jeonghoonji <wlwjdgns0920@naver.com>
2023-01-02 21:10:28 +0900

    [Fix] 장바구니 로그인 유무 확인

--- big-project-a-customer-ios/big-project-a-customer-ios/FolderShoppingBack/ShoppingBack/ShoppingBackView.swift
+++ big-project-a-customer-ios/big-project-a-customer-ios/FolderShoppingBack/ShoppingBack/ShoppingBackView.swift
@@ -118,64 +118,87 @@ struct ShoppingBackView: View {
                 
                 Spacer()
                 
-                // MARK: body
-                ScrollView(showsIndicators: false) {
-                    ForEach($vm.sCItems) { item in
-                        ShoppingBackDetailView(item: item, vm: vm)
-                            .padding(.vertical)
-                        
-                        Divider()
-                    }
-                }
-                .padding(.horizontal, 20)
-                
-                // MARK: tail
-                Section {
-                    VStack {
-                        HStack {
-                            Text("총 상품 금액")
-                            Spacer()
-                            Text("\(totalPrice)원")
-                        }
-                        HStack {
-                            Text("총 배송비")
-                            Spacer()
-                            Text("+ \(totalPrice == 0 ? 0 : shippingCost)원")
-                        }
-                        Divider()
-                        HStack {
-                            Text("결제 금액")
-                            Spacer()
-                            Text("\(totalPrice == 0 ? 0 : totalPrice+shippingCost)원")
-                        }
-                        .font(.title2.bold())
-                        
-                        HStack {
-
-                            NavigationLink(destination: {
-								OrderSheetAddress(totalPriceForBinding: $totalPriceForBinding)
-                            }, label: {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .frame(width: UIScreen.main.bounds.width - 40, height: 50)
-                                    Text("구매하기 (\(totalCount))")
-                                        .fontWeight(.bold)
-                                        .foregroundColor(.white)
-
-                                }
-
-                            }
+                // FIXME: 민주님 확인 부탁들입니다!!
+                // 로그인에 성공하면 userEmail이 nil이 아니므로 OrderSheetAddress뷰로 이동한다.
+                if signUpViewModel.currentUser?.userEmail != nil {
+                    // MARK: body
+                    ScrollView(showsIndicators: false) {
+                        ForEach($vm.sCItems) { item in
+                            ShoppingBackDetailView(item: item, vm: vm)
+                                .padding(.vertical)
                             
-                        )}
+                            Divider()
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    // MARK: tail
+                    Section {
+                        VStack {
+                            HStack {
+                                Text("총 상품 금액")
+                                Spacer()
+                                Text("\(totalPrice)원")
+                            }
+                            HStack {
+                                Text("총 배송비")
+                                Spacer()
+                                Text("+ \(totalPrice == 0 ? 0 : shippingCost)원")
+                            }
+                            Divider()
+                            HStack {
+                                Text("결제 금액")
+                                Spacer()
+                                Text("\(totalPrice == 0 ? 0 : totalPrice+shippingCost)원")
+                            }
+                            .font(.title2.bold())
+                            
+                            HStack {
+                                    NavigationLink(destination: {
+                                        OrderSheetAddress(totalPriceForBinding: $totalPriceForBinding)
+                                    }, label: {
+                                        ZStack {
+                                            RoundedRectangle(cornerRadius: 5)
+                                                .frame(width: UIScreen.main.bounds.width - 40, height: 50)
+                                            Text("구매하기 (\(totalCount))")
+                                                .fontWeight(.bold)
+                                                .foregroundColor(.white)
+                                            
+                                        }
+                                        
+                                    }
+                                                   
+                                    )
+
+                                
+                                
+                            }
+                        }
+                    }
+                    .padding()
+                    .background {
+                        Color.gray.brightness(0.4)
+                    }
+                    .sheet(isPresented: $isShowingLoginSheet) {
+                        LoginView()
+                    }
+                }else { // userEmail이 nil이면 로그인을 하지 않은 상태이므로 LoginView를 띄운다.
+                    Button {
+                        isShowingLoginSheet.toggle()
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(maxWidth: 200, maxHeight: 50)
+                            Text("바로구매")
+                                .foregroundColor(.white)
+                        }
+                        
                     }
                 }
-                .padding()
-                .background {
-                    Color.gray.brightness(0.4)
-                }
-                .sheet(isPresented: $isShowingLoginSheet) {
-                    LoginView()
-                }
+                
+               
+                
+                
             }
             .navigationBarTitle("장바구니")
             .navigationBarTitleDisplayMode(.automatic)
