84f9fdc9b54ed8c3dd0233f72096ceb6fe341a9b
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-03 17:24:59 +0900

    [Feat] OrderItem의 options 타입 변환 기능 구현

--- big-project-a-customer-ios/big-project-a-customer-ios/Store/OrderItemStore.swift
+++ big-project-a-customer-ios/big-project-a-customer-ios/Store/OrderItemStore.swift
@@ -15,6 +15,8 @@ class OrderItemStore: ObservableObject {
     
     let firebasePath = Firestore.firestore()
     
+    
+    
     // MARK: - Create New Customer(user)
     /// Auth에 새로운 사용자를 생성합니다.
     /// - Parameter email: 입력받은 사용자의 email
@@ -24,10 +26,26 @@ class OrderItemStore: ObservableObject {
     // MARK: - Request ShoppingBag Items
     /// uid 값을 통해 database의 특정 uid에 저장된 장바구니 아이템을 요청합니다.
     /// - Parameter uid: 로그인한 사용자의 uid
-    func requestShoppingList(uid: String) async -> Void {
+    func createShoppingItem(uid: String, item: OrderItemInfo) {
         
-        do {
-            let snapshot = try await firebasePath.document(uid).collection("MyCart").getDocuments()
+        firebasePath.document(uid).collection("MyCart").addDocument(data: [
+            "itemuid": item.itemuid,
+            "storeId": item.storeId,
+            "itemName": item.itemName,
+            "itemImage": item.itemImage,
+            "price": item.price,
+            "deliveryStatus": item.deliveryStatus.rawValue,
+            "option": item.option
+        ])
+    }
+    
+    
+    // MARK: - Request ShoppingBag Items
+    /// uid 값을 통해 database의 특정 uid에 저장된 장바구니 아이템을 요청합니다.
+    /// - Parameter uid: 로그인한 사용자의 uid
+    func requestShoppingList(uid: String) async -> Void {
+        do {"C"
+            let snapshot = try await firebasePath.collection("CustomerInfo").document(uid).collection("MyCart").getDocuments()
 
             for document in snapshot.documents {
                 let requestedData = document.data()
@@ -37,8 +55,12 @@ class OrderItemStore: ObservableObject {
                 let itemName: String = requestedData["itemName"] as? String ?? ""
                 let itemImage: String = requestedData["itemImage"] as? String ?? ""
                 let price: Int = requestedData["price"] as? Int ?? 0
-                let deliveryStatus: DeliveryStatusEnum = requestedData["deliveryStatus"] as? DeliveryStatusEnum ?? .pending
-                let option: [String: (String, Int)] = requestedData["option"] as? [String: (String, Int)] ?? [:]
+                // 배송상태가 서버에 열거형의 rawvalue로 저장되기 때문에 열거형 타입으로 변환
+                let deliveryStrings = requestedData["deliveryStatus"] as? String ?? "pending"
+                let deliveryStatus: DeliveryStatusEnum = DeliveryStatusEnum(rawValue: deliveryStrings) ?? .pending
+
+                let option: [String: String] = requestedData["option"] as? [String: String] ?? [:]
+                let newOptions = changeOptionsForm(option: option)
                 
                 dump("\(itemuid), \(storeId), \(itemName), \(itemImage), \(price), \(deliveryStatus), \(option)")
                 
@@ -48,13 +70,33 @@ class OrderItemStore: ObservableObject {
                                               itemImage: itemImage,
                                               price: price,
                                               deliveryStatus: deliveryStatus,
-                                              option: option)
-                self.items.append(orderItem)
+                                              option: newOptions)
+                
+                // Main Thread에서 Published 프로퍼티 변경
+                await MainActor.run(body: {
+                    self.items.append(orderItem)
+                })
+                
+                print("orderItem: \(orderItem)")
             }
             
         } catch {
+            print("Read Error")
             dump("ERROR SHOPPINGBAGS ITEMS REQUEST FALIED : \(error.localizedDescription)")
         }
+    }
+    
+    // MARK: - 옵션의 형태를 변환하는 메소드
+    /// 서버에 저장된 형태는 [색상: 노란색_100, 크기: s_1000 ]이다.
+    /// 그러므로 [String: (String, Int)]형태로 변환하는 작업을 수행해야 한다.
+    /// 아래는 변환 작업을 수행함
+    func changeOptionsForm(option: [String: String]) -> [String: (String, Int)] {
+        var newOptions: [String: (String, Int)] = [:]
+        option.forEach { dict in
+            let split = dict.value.components(separatedBy: "_")
+            newOptions[dict.key] = (split.first ?? "null", Int(split.last ?? "0") ?? 0)
+        }
         
+        return newOptions
     }
 }
