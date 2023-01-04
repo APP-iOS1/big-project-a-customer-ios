9bb646c239d267d64104a970735d3837d32e1fc2
ehvkddl <ehvkddl@naver.com>
2023-01-04 14:09:48 +0900

    [feat] 비밀번호 변경 시 조건 확인, 회원정보 변경시 View 전환 #5

--- big-project-a-customer-ios/big-project-a-customer-ios/FolderMyPage/FolderMyPageEditInfo/MyPageInfoEditView.swift
+++ big-project-a-customer-ios/big-project-a-customer-ios/FolderMyPage/FolderMyPageEditInfo/MyPageInfoEditView.swift
@@ -13,10 +13,22 @@ struct MyPageInfoEditView: View {
     @EnvironmentObject private var signupViewModel: SignUpViewModel
     @State var newPassword = ""
     @State var checkPassword = ""
+    @State private var isSecuredPassword = true
+    @State private var isSecuredPasswordCheck = true
+    @FocusState var isInFocusPassword: Bool
+    @FocusState var isInFocusPasswordCheck: Bool
     @State var newAddress = ""
     @State var newPhoneNumber = ""
+    @Binding var navStack: NavigationPath
 
     @State var showingAlert = false
+    @State var showingLogoutAlert = false
+    
+    // 비밀번호 정규표현식을 검사하는 함수입니다.
+    func checkPasswordRule(password : String) -> Bool {
+        let regExp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,20}$"
+        return password.range(of: regExp, options: .regularExpression) != nil
+    }
     
     var body: some View{
         VStack{
@@ -27,42 +39,142 @@ struct MyPageInfoEditView: View {
                 .font(.title3)
                 .bold()
                 .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal, 30)
-            // 수정할 비밀번호를 입력하는 텍스트필드
-            SecureField("Input new password.", text: $newPassword)
-                .modifier(InputModifier())
-            // 수정할 비밀번호를 한 번 더 확인하는 텍스트필드
-            SecureField("One more time.", text: $checkPassword)
-                .modifier(InputModifier())
-            
-            
-            Button {
+                .padding(.horizontal, 15)
+                .padding(.bottom, 30)
+        
+            VStack(spacing: 30) {
+                VStack(alignment: .leading, spacing: 5) {
+                    HStack {
+                        // 비밀번호 숨김 아이콘일 때
+                        if isSecuredPassword {
+                            SecureField("비밀번호를 입력해주세요.", text: $newPassword)
+                                .focused($isInFocusPassword) // 커서가 올라가있을 때 상태를 저장.
+                                .modifier(ClearTextFieldModifier())
+                        } else { // 비밀번호 보임 아이콘일 때
+                            TextField("비밀번호를 입력해주세요.", text: $newPassword)
+                                .focused($isInFocusPassword)
+                                .modifier(ClearTextFieldModifier())
+                        }
+                        
+                        Button(action: {
+                            // 비밀번호 보임/숨김을 설정함.
+                            isSecuredPassword.toggle()
+                        }) {
+                            Image(systemName: self.isSecuredPassword ? "eye.slash" : "eye")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20.5)
+                                .accentColor(.gray)
+                        }
+                        // password가 비어있지 않으면서, 6자리 이상일 때 체크 아이콘 띄움.
+                        if !newPassword.isEmpty && checkPasswordRule(password: newPassword) {
+                            Image(systemName: "checkmark.circle")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20.5)
+                                .foregroundColor(.green)
+                        }
+                    } // HStack - TextField, Secured Image, Check Image
+                    .frame(height: 30) // TextField가 있는 HStack의 height 고정 <- 아이콘 크기 변경 방지
+                    .padding(.trailing, 20)
+                    
+                    Rectangle()
+                        .modifier(TextFieldUnderLineRectangleModifier(stateTyping: isInFocusPassword))
+                    
+                    // 비밀번호 형식이 아닐 경우 경고 메시지
+                    if !newPassword.isEmpty && !checkPasswordRule(password: newPassword) {
+                        HStack(alignment: .center, spacing: 5) {
+                            Image(systemName: "exclamationmark.circle")
+                            Text("영문, 숫자, 특수문자를 포함하여 8~20자로 작성해주세요.")
+                        }
+                        .foregroundColor(.red)
+                        .font(.caption)
+                        .padding(.horizontal, 20)
+                    } else {
+                        Text("") // TextField 자리 고정
+                    }
+                } // VStack - HStack과 밑줄 Rectangle
+                .frame(height: 30)
                 
-                /// 비밀번호가 공백이거나, 엔터가 눌린 상태에서 변경 버튼이 눌리지 않게끔하는 조건을 걸고
-                /// 그 안에 두 번 입력한 비밀번호가 같을 경우 비밀번호 변경을 시행 가능하게 함
-//                if newPassword.trimmingCharacters(in: .whitespacesAndNewlines).count > 0 {
-//                    // 비밀번호가 같을 때 비밀번호 변경할 수 있는 조건
-//                    if newPassword == checkPassword {
-//                        // 새로 입력한 비밀번호를 저장
-//                        vm.users.userPassward = newPassword
-//                    } else {
-//                        // 입력한 두 비밀번호가 다를 때 alert를 띄워서
-//                        // 일치하지 않다는 것을 알림
-//                        showingAlert = true
-//                    }
-//                }
-                if newPassword == checkPassword {
-                    signupViewModel.updatePassword(password: newPassword)
+                VStack(alignment: .leading, spacing: 5) {
+                    HStack {
+                        // 비밀번호 숨김 아이콘일 때
+                        if isSecuredPasswordCheck {
+                            SecureField("비밀번호를 한번 더 입력해 주세요.", text: $checkPassword)
+                                .focused($isInFocusPasswordCheck) // 커서가 올라가있을 때 상태를 저장.
+                                .modifier(ClearTextFieldModifier())
+                        } else { // 비밀번호 보임 아이콘일 때
+                            TextField("비밀번호를 한번 더 입력해 주세요.", text: $checkPassword)
+                                .focused($isInFocusPasswordCheck)
+                                .modifier(ClearTextFieldModifier())
+                        }
+                        
+                        Button(action: {
+                            isSecuredPasswordCheck.toggle()
+                        }) {
+                            Image(systemName: self.isSecuredPasswordCheck ? "eye.slash" : "eye")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20.5)
+                                .accentColor(.gray)
+                        }
+                        
+                        
+                        // passwordCheck가 비어있지 않으면서, password와 같으면 체크 아이콘 띄움.
+                        if !checkPassword.isEmpty && newPassword == checkPassword && checkPasswordRule(password: checkPassword) {
+                            Image(systemName: "checkmark.circle")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20.5)
+                                .foregroundColor(.green)
+                        }
+                    } // HStack - TextField, Secured Image, Check Image
+                    .frame(height: 30) // TextField가 있는 HStack의 height 고정 <- 아이콘 크기 변경 방지
+                    .padding(.trailing, 20)
+                    
+                    Rectangle()
+                        .modifier(TextFieldUnderLineRectangleModifier(stateTyping: isInFocusPasswordCheck))
+                    
+                    // 비밀번호가 같지 않을 경우 경고 메시지
+                    if !checkPassword.isEmpty && newPassword != checkPassword {
+                        HStack(alignment: .center, spacing: 5) {
+                            Image(systemName: "exclamationmark.circle")
+                            Text("비밀번호를 다시 입력해주세요.")
+                        }
+                        .foregroundColor(.red)
+                        .font(.caption)
+                        .padding(.horizontal, 20)
+                    } else {
+                        Text("") // TextField 자리 고정
+                    }
+                } // VStack - HStack과 밑줄 Rectangle
+                .frame(height: 30)
+                Button {
+                    /// 비밀번호가 공백이거나, 엔터가 눌린 상태에서 변경 버튼이 눌리지 않게끔하는 조건을 걸고
+                    /// 그 안에 두 번 입력한 비밀번호가 같을 경우 비밀번호 변경을 시행 가능하게 함
+                    if !newPassword.isEmpty && checkPasswordRule(password: newPassword) {
+                        signupViewModel.updatePassword(password: newPassword)
+                        showingLogoutAlert = true
+                    } else {
+                        showingAlert = true
+                    }
+                    newPassword = ""
+                    checkPassword = ""
+                } label: {
+                    Text("비밀번호 변경")
+                        .frame(width: 330)
+                        .modifier(ColoredButtonModifier(cornerRadius: 10))
                 }
-                
-                
-            } label: {
-                Text("비밀번호 변경")
-                    .modifier(ConfirmModifier())
+                // alert: 입력한 두 비밀번호가 일치하지 않을 때 알림
+                // ok버튼을 누르면 텍스트필드 초기화
+                .modifier(PasswordAlertModifier(showingAlert: $showingAlert, password: $newPassword, password_2: $checkPassword))
+                .alert("비밀번호가 변경되어, 보안상 고객님의 모든 기기에서 로그아웃 됩니다.", isPresented: $showingLogoutAlert) {
+                    Button("Ok") {
+                        signupViewModel.requestUserSignOut()
+                        navStack = .init()
+                    }
+                }
             }
-            // alert: 입력한 두 비밀번호가 일치하지 않을 때 알림
-            // ok버튼을 누르면 텍스트필드 초기화
-            .modifier(PasswordAlertModifier(showingAlert: $showingAlert, password: $newPassword, password_2: $checkPassword))
             
             Divider()
                 .padding(.vertical, 10)
@@ -72,10 +184,10 @@ struct MyPageInfoEditView: View {
                 .font(.title3)
                 .bold()
                 .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal, 30)
+                .padding(.horizontal, 15)
             
             
-            VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .leading, spacing: 20) {
                 HStack(spacing: 20) {
                     Text("고객명")
                         .foregroundColor(.secondary)
@@ -91,50 +203,58 @@ struct MyPageInfoEditView: View {
                 HStack(spacing: 20) {
                     Text("연락처")
                         .foregroundColor(.secondary)
-                    TextField("전화번호를 등록해주세요", text: $newPhoneNumber)
-                        .modifier(InputModifier(padding: 0))
-                        .keyboardType(.numberPad)
-
-                    
+                    VStack {
+                        TextField("전화번호를 등록해주세요", text: $newPhoneNumber)
+                            .keyboardType(.numberPad)
+                        Rectangle()
+                            .modifier(TextFieldUnderLineRectangleModifier(stateTyping: isInFocusPassword, padding: 0))
+                    }
                 }
+                
                 HStack(spacing: 34.5) {
                     Text("주소")
                         .foregroundColor(.secondary)
-                    TextField("주소를 등록해주세요", text: $newAddress) 
-                        .modifier(InputModifier(padding: 0))
+                    VStack {
+                        TextField("주소를 등록해주세요", text: $newAddress)
+                        Rectangle()
+                            .modifier(TextFieldUnderLineRectangleModifier(stateTyping: isInFocusPassword, padding: 0))
+                    }
                 }
+                
             }
             .onAppear {
                 newPhoneNumber = signupViewModel.currentUser?.phoneNumber ?? ""
                 newAddress = signupViewModel.currentUser?.userAddress ?? ""
             }
-            .padding(.horizontal, 30)
-            .padding(.top, 5)
+            .padding(.horizontal, 20)
+            .padding(.vertical, 20)
             
             // 주소 변경 버튼
             Button {
                 // 입력한 텍스트가 띄어쓰기나, 엔터 버튼이 눌렸을 때 버튼이 작동되지 않는 조건
-//                if newAddress.trimmingCharacters(in: .whitespacesAndNewlines).count > 0 {
-//                    // 새로 입력한 주소를 저장
-//                    vm.users.userAddress = newAddress
-//                }
-                signupViewModel.updateUserInfo(userAddress: newAddress, phoneNumber: newPhoneNumber, user: signupViewModel.currentUser!)
-                
+                if newAddress.trimmingCharacters(in: .whitespacesAndNewlines).count > 0 && newPhoneNumber.trimmingCharacters(in: .whitespacesAndNewlines).count > 0 {
+                    signupViewModel.updateUserInfo(userAddress: newAddress, phoneNumber: newPhoneNumber, user: signupViewModel.currentUser!)
+                    navStack = .init()
+                }
             } label: {
                 Text("수정하기")
-                    .modifier(ConfirmModifier())
+                    .frame(width: 330)
+                    .modifier(ColoredButtonModifier(cornerRadius: 10))
             }
+            .padding(.top, 10)
+            
             Spacer()
         }
         .padding(.top, 10)
         .navigationBarTitle("회원정보 수정")
+
     }
 }
 
 struct MyPageInfoEditView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
-            MyPageInfoEditView()
+            MyPageInfoEditView(navStack: .constant(NavigationPath()))
                 .environmentObject(SignUpViewModel())
 
         }
