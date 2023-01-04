e352797590d4da119de16f8f7e603947caefd3c9
jeonghoonji <wlwjdgns0920@naver.com>
2023-01-04 15:45:36 +0900

    [Add]리뷰 추가 코드

--- big-project-a-customer-ios/big-project-a-customer-ios/Model/ItemInfo.swift
+++ big-project-a-customer-ios/big-project-a-customer-ios/Model/ItemInfo.swift
@@ -7,8 +7,9 @@ import Foundation
 /// 판매자가 아이템의 옵션을 직접 추가할 수 있도록 준비된 커스텀 타입입니다.
 /// 사용자가 좌측에 입력한 키(key)가 Firebase 문서에 없다면, 자동으로 해당 데이터를 업데이트 합니다.
 /// 모든 키의 밸류(value)는 배열로 업데이트 됩니다.
-struct ItemInfo: Codable {
+struct ItemInfo: Identifiable,Codable {
 //    var itemUid: String
+    var id : UUID = UUID()
     var itemId: String
     var storeId: String
     var itemName: String
